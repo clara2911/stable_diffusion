4f77e775ebcd7b755bc0c66e4d38f959e206350a
Clara Tump <clara.tump@snappet.org>
2022-10-28 23:23:19 +0200

    move from notebooks to script

---
+++ generate_storybook.py
@@ -0,0 +1,10 @@
+
+
+
+# start from a sentence
+# complete the story
+# (possible by feeding it to gpt3 multiple times
+# by generate > summarize > generate > summarize)
+# visually summarize each 'page'
+# create an image for each page
+# print each page of story + image on a pdf
---
+++ src/illustration_generator.py
@@ -0,0 +1,13 @@
+class IllustrationGenerator:
+
+    def __init__(self):
+        pass
+
+    def _pass_huggingface_access_token(self):
+        pass
+
+    def generate_image_from_prompt(self, prompt):
+        pass
+
+    def _generate_stable_diffusion_response(self, prompt):
+        pass
